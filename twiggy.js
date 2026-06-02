da639a476b75986ff74b1e2f8be6a7ae907e4919
Bhargav Bora <bhargavbora79@gmail.com>
2026-06-02 15:00:41 +0530

    added some modification

--- twiggy.js
+++ twiggy.js
@@ -1,26 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logo from "./assets/twiggy-logo.png";
 
-const header =()=>{
+
+
+const Header =()=>{
     return(
         <div className="header">
-            <div className="logo container">
-              <img
-              className="logo"
-              src={logo} alt="Twiggy Logo"
+            <div className="logo-container">
+               <img className="logo"
+                 src="https://images-platform.99static.com/v84irzbNBd5aawXGKXfH4SEjcn0=/0x0:960x960/500x500/top/smart/99designs-contests-attachments/117/117132/attachment_117132760"
               />
+              
             </div>
-            
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Cart</li>
+                    <li>Help</li>
+                    <li>Account</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
 
+
+
+
 const AppLayout = ()=>{
     return(
         <div className ="app">
-         <header/>
+         <Header/>
+         
         </div>
     )
 }
