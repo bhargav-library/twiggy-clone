dcf346c8b75cf5d5be94199b7d9ceee0d0851dd3
Bhargav Bora <bhargavbora79@gmail.com>
2026-06-02 12:01:12 +0530

    added logo using functional component.

--- twiggy.js
+++ twiggy.js
@@ -1,5 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./assets/twiggy-logo.png";
+
+const header =()=>{
+    return(
+        <div className="header">
+            <div className="logo container">
+              <img
+              className="logo"
+              src={logo} alt="Twiggy Logo"
+              />
+            </div>
+            
+        </div>
+    )
+}
 
 
-console.log("hello");
+const AppLayout = ()=>{
+    return(
+        <div className ="app">
+         <header/>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
