bb0447ece7b9fabd0f5a9a87fd05bf1a4be39936
Bhargav Bora <bhargavbora79@gmail.com>
2026-06-06 18:01:37 +0530

    completed the body

--- twiggy.js
+++ twiggy.js
@@ -609,7 +609,7 @@ const RestauranrCard=(props)=>{
       src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}
       />
       <h3>{name}</h3>
-      <h4>{cuisines}</h4>
+      <h4>{cuisines.join(", ")}</h4>
        <h4>{costForTwo}</h4>   
       <h4>{sla?.deliveryTime} minutes</h4>
     </div>
