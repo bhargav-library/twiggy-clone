181719858c8f7c2021406f486c0f7776c197dc6b
Bhargav Bora <bhargavbora79@gmail.com>
2026-06-17 15:20:12 +0530

    added filter feature

--- src/body.js
+++ src/body.js
@@ -1,24 +1,36 @@
 import reslist from "./utilities.js/mockdata";
 import RestaurantCard from "./restaurant component";
+import { useState } from "react";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(reslist);
+
   return (
     <div className="body">
       <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search restaurants..."
-        />
+        <input type="text" className="search-input" placeholder="Search restaurants..." />
         <button className="search-btn">Search</button>
       </div>
-      <div className="restaurant-list">
-        {reslist.map((restaurant)=>(
-          <RestaurantCard key={restaurant.info.id}  resData={restaurant}/>
-        ))}
+
+      <div className="filtered-list">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
 
+      <div className="restaurant-list">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Body;
+
---
+++ src/filter.js
@@ -0,0 +1,17 @@
+const Filter = ({ listOfRestaurants, setListOfRestaurants }) => {
+  return (
+    <div className="filtered-list">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+          setListOfRestaurants(filteredList);
+        }}
+      >
+        Top Rated Restaurants
+      </button>
+    </div>
+  );
+};
+
+export default Filter;
